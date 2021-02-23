0b467d9373faf2a6f064beb63d1f600ec35e16bb
mihai <mihai.iusan@gmail.com>
2021-02-23 14:06:09 +0100

    switched ChuckNorrisQuotes as a Bean Configuration from Java based Configuration to XML based configuration. This is just as an example as it is preferred to use Java based config.

--- src/main/java/guru/springframework/spring5jokesappv2/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/spring5jokesappv2/config/ChuckConfiguration.java
@@ -1,14 +1,21 @@
 package guru.springframework.spring5jokesappv2.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/**
+ * Java Based Bean configuration is skipped to XML based. This is done to understand how the
+ * XML based configuration works as you can find it in older projects.
+ * To Switch back, you need to:
+ *  - remove @ImportResource annotation that refers to the XML that contains "ChuckNorrisQuotes" configuration
+ *    from Spring5JokesAppV2Application
+ *  - remove the XML that contains "ChuckNorrisQuotes"(chuck-config.xml)
+ *  - Re-add @Configuration and @Bean annotations
+ */
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
-    public ChuckNorrisQuotes chuckNorrisQuotes(){
+    //@Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
 }
